e21a731e583dbc15b090ce5710b6809fd41a9a56
Kailey Waal <15kwaal@gmail.com>
2022-12-04 15:23:45 -0500

    login and navigation

--- login-template/src/components/App.js
+++ login-template/src/components/App.js
@@ -3,13 +3,20 @@ import Signup from "./Signup"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext";
+import { Route, Routes } from "react-router-dom"
+import Dashboard from './Dashboard'
+import Login from './Login'
 
 function App() {
   return (
     <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{minHeight: "100vh"}}>
         <div className="w-100" style={{maxWidth: "400px"}}>
-        <Signup/>
+          <Routes>
+            <Route exact path="/" element={<Dashboard/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/login" element={<Login/>}/>
+          </Routes>
         </div>
       </Container>
     </AuthProvider>
--- login-template/src/contexts/AuthContext.js
+++ login-template/src/contexts/AuthContext.js
@@ -9,14 +9,20 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
 
     function signup(email, password) {
         return auth.createUserWithEmailAndPassword(email, password)
     }
 
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
+            setLoading(false)
         })
 
         return unsubscribe
@@ -26,12 +32,13 @@ export function AuthProvider({ children }) {
 
     const value = {
         currentUser,
-        signup
+        signup,
+        login
     }
 
     return (
         <AuthContext.Provider value={value}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     )
 }
